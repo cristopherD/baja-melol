d4dc38546826c7528d9e04ce42765cef7a0760fa
al037843 <al037843@S04-PC04>
2016-01-13 17:22:19 +0100

    version inicial proyecto_crud

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    <html> <h3> Se devuelven los jugadores asignado al coach a la lista de disponibles de la liga a la que pertenece.</h3>
+        <h3> Se eliminaran los registros de puntuación del coach.</h3>
+        <h3> Eliminar las referencias al coach de las diferentes tablas.</h3>
+        <h3> Se expulsara de la liga a la que esta unida.</h3>
+        <h3> Si es el administrador de la liga estos privilegios pasarían al segundo con mas antigüedad.</h3>
+        <h3> ? Si es el ultimo coach de una liga esta sera borrada.</h3>
+        <h3> ?si el coach se da de baja no puede volver a entrar en la liga </h3>
+        
+    </html>
+    </body>
+</html>
